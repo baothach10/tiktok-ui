7aa3f6971cf47b67fd9dc8c482e7ef6e4ece45b3
NgoTranBaoThach <s3927021@rmit.edu.vn>
2024-01-08 15:00:03 +0700

    Add layout and pages to the web application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component
+            let Layout = DefaultLayout
+            if (route.layout) {
+              Layout = route.layout
+
+            } else if (route.layout === null) {
+              Layout = Fragment
+            }
+            return <Route key={index} path={route.path} element={<Layout><Page /></Layout>}></Route>
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,16 @@
+import Header from './Header'
+
+function HeaderOnly({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className='container'>
+                <div className='content'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeaderOnly;
