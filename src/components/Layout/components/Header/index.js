73b94726e0c460f8f729c2fe2a038f0c73e1c221
NgoTranBaoThach <s3927021@rmit.edu.vn>
2024-01-08 17:56:13 +0700

    Create logo for header, add css for search bar

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,30 @@
 import styles from './header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles)
 
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-            
+            <div className={cx('logo')}>
+                <img src={images.logo} alt='TikTok logo'></img>
+            </div>
+            <div className={cx('search')}>
+                <input placeholder='Search accounts and videos' spellCheck={false} />
+                <button className={cx('clear')}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx('actions')}></div>
         </div>
     </header>
 }
