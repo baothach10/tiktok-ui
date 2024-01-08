8516fd6dd13036e7a48ae8b6d565f8c2e8284c37
NgoTranBaoThach <s3927021@rmit.edu.vn>
2024-01-08 15:03:58 +0700

    Move Header into Component folder

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,4 +1,4 @@
-import Header from './Header'
+import Header from '../components/Header'
 import Sidebar from './Sidebar'
 
 function DefaultLayout({children}) {
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,4 +1,4 @@
-import Header from './Header'
+import Header from '../components/Header'
 
 function HeaderOnly({ children }) {
     return (
