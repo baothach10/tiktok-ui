547bef302c09f4cac3d12245bb7b95843572bb97
baothach10 <baothach10@gmail.com>
2024-01-12 16:39:27 +0700

    Add account preview to each account on sidebar and add styles to each of them

--- src/components/SuggestedAccounts/SuggestedAccountItem.js
+++ src/components/SuggestedAccounts/SuggestedAccountItem.js
@@ -1,28 +1,50 @@
-import PropTypes from 'prop-types'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tippy from '@tippyjs/react/headless';
 
 import styles from './SuggestedAccounts.module.scss'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import SuggestedAccountPreview from './SuggestedAccountPreview';
 
 const cx = classNames.bind(styles)
 
 function SuggestedAccountItem() {
-    return (
-        <div className={cx('account-item')}>
-            <img
-                className={cx('avatar')}
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png" 
-                alt='user'
-            />
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>ABC</strong>
-                    <FontAwesomeIcon className={cx('checked')} icon={faCheckCircle} />
-                </p>
-                <p className={cx('name')}>ABC</p>
+    const renderPreview = (props) => {
+        return (
+            <div className={cx('preview')} tabIndex='-1' {...props}>
+                <PopperWrapper>
+                    <SuggestedAccountPreview/>
+                </PopperWrapper>
             </div>
+        )
+    }
 
+    return (
+        <div>
+            <Tippy
+                offset={[-25,0]}
+                interactive
+                delay={[800, 0]}
+                placement='bottom'
+                render={renderPreview}
+            >
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png"
+                        alt='user'
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>ABC</strong>
+                            <FontAwesomeIcon className={cx('checked')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>ABC</p>
+                    </div>
+
+                </div>
+            </Tippy>
         </div>
     );
 }
