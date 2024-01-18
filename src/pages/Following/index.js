630cc22223fb527f4f330245ad36f855a2ef581f
baothach10 <baothach10@gmail.com>
2024-01-19 00:08:26 +0700

    Add the following page

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -1,5 +1,34 @@
+import classNames from "classnames/bind";
+
+import styles from './Following.module.scss';
+import Post from "~/layouts/components/Post";
+import { posts } from "src/fakedata";
+
+const cx = classNames.bind(styles)
+
 function Following() {
-    return <h2>Following</h2>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('container')}>
+                {posts.map(post => (
+                    <Post 
+                        key={post.id}
+                        id={post.id}
+                        avatar={post.avatar}
+                        nickname={post.nickname}
+                        fullName={post.fullName}
+                        title={post.title}
+                        music={post.music}
+                        video={post.video}
+                        likes={post.likes}
+                        comments={post.comments}
+                        saved={post.saved}
+                        share={post.share}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Following;
